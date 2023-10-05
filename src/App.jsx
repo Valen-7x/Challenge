4edadafdda7146755eafda60706cb9e9f8992fe4
Valen-7x <valen.deorigen@gmail.com>
2023-10-04 21:33:19 -0300

    styles changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,37 @@ import { useFetch } from './useFetch';
 
 function App() {
   const { data } = useFetch("https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_rfcCIPVpdQ7YZ8TLlNUrDckneefKvLmfn4BpYBacIHtGZF1g6zwUAyrkotqK1paP");
-  const [imageURL, setImageURL] = useState('');
+
+  const [currentIndex, setCurrentIndex] = useState(0);
   useEffect(() => {
     if (data && data.length > 0) {
-      setImageURL(data[0].url);
+      setCurrentIndex(0); // Restaura el índice a 0 cuando se carga la nueva lista de img
     }
   }, [data]);
-  // Función para cambiar la URL de la imagen
-  const changeImageURL = () => {
-    // Cambia la URL de la imagen (por ejemplo, a data[2].url)
-    if (data && data.length > 2) {
-      setImageURL(data[2].url);
+  console.log(data);
+
+  // Función para cambiar la URL de la imagen 
+  const changeImage = () => {
+    if (data && data.length > 0) {
+      const nextIndex = (currentIndex + 1) % data.length;
+      setCurrentIndex(nextIndex);
     }
   };
 
   return (
-    <div className='flex justify-center items-center min-h-screen bg-gray-300 rounded-2xl'>
+    <div className='flex justify-center items-center min-h-screen bg-gray-100'>
       {/* div centrado */}
-      <div className=' flex flex-col items-center bg-white rounded-lg p-4 shadow-md'>
+      <div className=' flex flex-col items-center rounded-lg p-4 shadow-md bg-gray-300'>
         <img
-          src={imageURL}
+          src={data?.[currentIndex]?.url}
           className='w-40 h-40 rounded-full mx-auto'
           alt=""
         />
         <div className='mt-4'>
-          <p className='w-[20rem]'>{data?.[0].breeds[0].description}</p>
+          <p className='w-[20rem]'>{data?.[currentIndex]?.breeds[0].temperament}</p>
         </div>
         {/* boton implementado con el hook useState */}
-        <button onClick={changeImageURL} className='rounded-full p-2 bg-violet-500 text-white hover:bg-blue-700 transition duration-300'>
+        <button onClick={changeImage} className='rounded-full p-2 bg-violet-500 text-white hover:bg-blue-700 transition duration-300'>
           Botón
         </button>
       </div>
