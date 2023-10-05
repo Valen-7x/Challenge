6ce8d19a18e6dc8f5ddc57383746a73dac46d2ca
Valen-7x <valen.deorigen@gmail.com>
2023-10-04 21:39:43 -0300

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,13 @@ import { useFetch } from './useFetch';
 
 function App() {
   const { data } = useFetch("https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_rfcCIPVpdQ7YZ8TLlNUrDckneefKvLmfn4BpYBacIHtGZF1g6zwUAyrkotqK1paP");
-
   const [currentIndex, setCurrentIndex] = useState(0);
+
   useEffect(() => {
     if (data && data.length > 0) {
       setCurrentIndex(0); // Restaura el índice a 0 cuando se carga la nueva lista de img
     }
   }, [data]);
-  console.log(data);
-
   // Función para cambiar la URL de la imagen 
   const changeImage = () => {
     if (data && data.length > 0) {
@@ -29,11 +27,12 @@ function App() {
           className='w-40 h-40 rounded-full mx-auto'
           alt=""
         />
-        <div className='mt-4'>
-          <p className='w-[20rem]'>{data?.[currentIndex]?.breeds[0].temperament}</p>
+        <div className='mt-2'>
+          {/* trae el temperamento de cada gato en este caso en la data aparece el mismo en todos los cats  */}
+          <b className='w-[10rem]'>{data?.[currentIndex]?.breeds[0].temperament} </b>
         </div>
-        {/* boton implementado con el hook useState */}
-        <button onClick={changeImage} className='rounded-full p-2 bg-violet-500 text-white hover:bg-blue-700 transition duration-300'>
+        {/* boton cambia el indice de currentIndex*/}
+        <button onClick={changeImage} className='rounded-full p-2 mt-[2rem] bg-violet-500 text-white hover:bg-blue-700 transition duration-300'>
           Botón
         </button>
       </div>
